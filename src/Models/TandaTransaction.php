421b1ebb35c2134d609c11fa0e871adb0bb87834
edlugz <eddy.lugaye@gmail.com>
2024-01-26 22:52:49 +0300

    Update TandaTransaction.php
    -syntax corrections

--- src/Models/TandaTransaction.php
+++ src/Models/TandaTransaction.php
@@ -5,6 +5,10 @@ namespace EdLugz\Tanda\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static create(array|string[] $array_merge)
+ * @method update(array $array)
+ */
 class TandaTransaction extends Model
 {
 	use SoftDeletes;
