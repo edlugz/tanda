df45c051d24ed5583a31ce5e478fd46fb8241977
edlugz <eddy.lugaye@gmail.com>
2024-01-29 13:26:50 +0300

    Update TandaHelper.php
    - add to service provider function to cater for airtime

--- src/Helpers/TandaHelper.php
+++ src/Helpers/TandaHelper.php
@@ -9,37 +9,49 @@ use Illuminate\Http\Request;
 class TandaHelper
 {
     /**
-     * get service provider from mobile number.
+     * get mobile money / airtime provider from mobile number.
      *
      * @param string $mobileNumber - 0XXXXXXXXX
-     *
+     * @param bool $airtime
      * @return string
      */
-    public static function serviceProvider(string $mobileNumber): string
+    public static function serviceProvider(string $mobileNumber, bool $airtime = false): string
     {
         $safaricom = '/(?:0)?((?:(?:7(?:(?:[01249][0-9])|(?:5[789])|(?:6[89])))|(?:1(?:[1][0-5])))[0-9]{6})$/';
         $airtel = '/(?:0)?((?:(?:7(?:(?:3[0-9])|(?:5[0-6])|(8[5-9])))|(?:1(?:[0][0-2])))[0-9]{6})$/';
         $telkom = '/(?:0)?(77[0-9][0-9]{6})/';
-        $equitel = '/(?:0)?(76[3-6][0-9]{6})/';
-        if (preg_match($safaricom, $mobileNumber)) {
-            return 'MPESA';
-        } elseif (preg_match($airtel, $mobileNumber)) {
-            return 'AIRTELMONEY';
-        } elseif (preg_match($telkom, $mobileNumber)) {
-            return 'TKASH';
-        } elseif (preg_match($equitel, $mobileNumber)) {
-            return 'EQUITEL';
-        } else {
-            return '0';
+        $equitel = '/0?(76[3-6][0-9]{6})/';
+		if(!$airtime){
+			if (preg_match($safaricom, $mobileNumber)) {
+				return 'MPESA';
+			} elseif (preg_match($airtel, $mobileNumber)) {
+				return 'AIRTELMONEY';
+			} elseif (preg_match($telkom, $mobileNumber)) {
+				return 'TKASH';
+			} elseif (preg_match($equitel, $mobileNumber)) {
+				return 'EQUITEL';
+			} else {
+				return '0';
+			}
+		} else {
+			if (preg_match($safaricom, $mobileNumber)) {
+				return 'SAFARICOM';
+			} elseif (preg_match($airtel, $mobileNumber)) {
+				return 'AIRTEL';
+			} elseif (preg_match($telkom, $mobileNumber)) {
+				return 'TELKOM';
+			} else {
+				return '0';
+			}
         }
     }	
 	
     /**
      * Process payout results.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      *
-     * @return \EdLugz\Tanda\Models\TandaTransaction
+     * @return TandaTransaction
      */
     public function payout(Request $request): TandaTransaction
     {
@@ -88,7 +100,7 @@ class TandaHelper
     /**
      * Process c2b results.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      *
      * @return TandaFunding
      */
