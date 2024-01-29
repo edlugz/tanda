22824f1288659f7d6a2c0f2d03d2f6ef6c825fbf
edlugz <eddy.lugaye@gmail.com>
2024-01-29 22:20:32 +0300

    Update TandaFunding.php
    - method declaration

--- src/Models/TandaFunding.php
+++ src/Models/TandaFunding.php
@@ -5,6 +5,10 @@ namespace EdLugz\Tanda\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static where(string $string, $input)
+ * @method static create(array $array)
+ */
 class TandaFunding extends Model
 {
 	use SoftDeletes;
