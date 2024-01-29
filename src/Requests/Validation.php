7979ae567ba402b32ecf545985bd3d9621cf5eb3
edlugz <eddy.lugaye@gmail.com>
2024-01-29 22:31:18 +0300

    Update Validation.php

--- src/Requests/Validation.php
+++ src/Requests/Validation.php
@@ -2,6 +2,7 @@
 
 namespace EdLugz\Tanda\Requests;
 
+use EdLugz\Tanda\Exceptions\TandaRequestException;
 use EdLugz\Tanda\TandaClient;
 
 class Validation extends TandaClient
@@ -20,31 +21,32 @@ class Validation extends TandaClient
     {		
 		$this->endPoint = 'registry/v1/countries/KE/mmos/';		
     }
-	
+
     /**
      * Till lookup
-   
-      	@param string mmoId - mobile money operator id (Mpesa,AirtelMoney,TKash)
-      	@param string till
-		
-		@return mixed
+     *
+     * @param string $mmoId - mobile money operator id (Mpesa,AirtelMoney,TKash)
+     * @param string $till
+     *
+     * @return string
+     * @throws TandaRequestException
      */
-    public function till($mmoId, $till)
+    public function till(string $mmoId, string $till): string
     {
-        return $this->call($this->endPoint.''.$mmoId.'/merchants/'.$till.'', [], 'GET');
+        return $this->call($this->endPoint .$mmoId.'/merchants/'.$till, [], 'GET');
     }
-	
+
     /**
      * Business number (paybill) lookup
-     
-      	@param string mmoId - mobile money operator id (Mpesa,AirtelMoney,TKash)
-      	@param string till
-		
-		@return mixed
+     *
+     * @param string $mmoId
+     * @param string $businessShortCode
+     * @return mixed
+     * @throws TandaRequestException
      */
-    public function paybill($mmoId, $businessShortCode)
+    public function paybill(string $mmoId, string $businessShortCode): string
     {
-        return $this->call($this->endPoint.''.$mmoId.'/businesses/'.$businessShortcode.'', [], 'GET');
+        return $this->call($this->endPoint .$mmoId.'/businesses/'.$businessShortCode, [], 'GET');
     }
 	
 }
