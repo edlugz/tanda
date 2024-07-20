9593bd018f64922e27eeb90ae20589b7a88ba911
edlugz <eddy.lugaye@gmail.com>
2024-07-20 08:36:14 +0300

    Update Transaction.php
    - update status function to return either tanda transaction or tanda funding depending on transaction type

--- src/Requests/Transaction.php
+++ src/Requests/Transaction.php
@@ -3,6 +3,7 @@
 namespace EdLugz\Tanda\Requests;
 
 use EdLugz\Tanda\Models\TandaTransaction;
+use EdLugz\Tanda\Models\TandaFunding;
 use EdLugz\Tanda\Exceptions\TandaRequestException;
 use EdLugz\Tanda\TandaClient;
 
@@ -39,44 +40,74 @@ class Transaction extends TandaClient
     /**
      * Transaction status query
 	 *
+	 * @param string $transactionType
 	 * @param string $reference
 	 *
-	 * @return TandaTransaction
+	 * @return TandaTransaction | TandaFunding
      *@throws  TandaRequestException
 	 *
 	 */
-    public function status(string $reference) : TandaTransaction
+    public function status(string $transactionType, string $reference) : TandaTransaction|TandaFunding  
     {
-		$transaction = TandaTransaction::where('payment_reference', $reference)->first();
-		
-		try {
-			
-			$response = $this->call($this->endPoint.''.$reference.'', [], 'GET');
-			
-		} catch(TandaRequestException $e){
-			$response = [
-                'status'         => $e->getCode(),
-                'responseCode'   => $e->getCode(),
-                'message'        => $e->getMessage(),
-            ];
-
-            $response = (object) $response;
-		}
-		
-		$data = [
-            'request_status'      => $response->status,
-            'request_message'       => $response->message,
-        ];
-
-        if ($response->status == '000001') {
-            $data = array_merge($data, [
-                'receipt_number' => $response->receiptNumber,
-				'transaction_receipt' => $response->resultParameters[0]['value'],
-				'timestamp' => $response->datetimeCompleted,
-            ]);
+        if($transactionType === 'funding'){
+            $transaction = TandaFunding::where('transaction_id', $reference)->first();
+        } else {
+		    $transaction = TandaTransaction::where('payment_reference', $reference)->first();
         }
+        if($transaction)
+            try {
+                
+                $response = $this->call($this->endPoint.''.$reference.'', [], 'GET');
+                
+            } catch(TandaRequestException $e){
+                $response = [
+                    'status'         => $e->getCode(),
+                    'responseCode'   => $e->getCode(),
+                    'message'        => $e->getMessage(),
+                ];
 
-        $transaction->update($data);
+                $response = (object) $response;
+            }
+            
+            $data = [
+                'request_status'      => $response->status,
+                'request_message'       => $response->message,
+            ];
+            
+            if($response->status == '000001'){
+            
+                $transactionReceipt = $response->receiptNumber;
+                
+                if($response->resultParameters){
+                    $params = $response->resultParameters;
+                    $keyValueParams = [];
+                    foreach ($params as $param) {
+                        $keyValueParams[$param['id']] = $param['value'];
+                    }
+
+                    $transactionReceipt = $keyValueParams['transactionRef'];
+                }
+                
+                $data = array_merge($data, [
+                    'request_status' => $response->status,
+                    'request_message' => $response->message,
+                    'receipt_number' => $response->receiptNumber,
+                    'transaction_reference' => $transactionReceipt,
+                    'timestamp' => $response->timestamp,
+                ]);
+
+            } else {
+
+                $data = array_merge($data, [
+                    'request_status' => $response->status,
+                    'request_message' => $response->message,
+                    'timestamp' => $response->timestamp,
+                ]);
+
+            }
+            
+
+            $transaction->update($data);
 
         return $transaction;
     }
